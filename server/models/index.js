db4fa7bf61220a0c40f62d72a959099d11fa63ba
FeliciaKh <feliciakh0428@gmail.com>
2020-09-15 11:58:14 -0400

    Create Thought Model

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,7 @@ const db = new Sequelize("postgres://localhost:5432/remesh-project", {
   logging: false
 });
 
-const Conversation = db.define('conversation', {
+const Conversation = db.define('conversations', {
     title: {
         type: Sequelize.STRING, 
         allowNull: false
@@ -15,6 +15,21 @@ const Conversation = db.define('conversation', {
 })
 
 const Message = db.define('messages', {
+    text: {
+        type: Sequelize.TEXT, 
+        allowNull: false
+    }, 
+    date: {
+        type: Sequelize.DATE,
+        defaultValue: Date.now()
+    }, 
+    time: {
+        type: Sequelize.TIME, 
+        defaultValue: currentTime()
+    }
+})
+
+const Thought = db.define('thoughts', {
     text: {
         type: Sequelize.TEXT, 
         allowNull: false
